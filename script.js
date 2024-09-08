33b0008097403308447c3e64428cc6bb037dd259
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-09-08 14:15:45 +0530

    created a basic To-Do List App as sample

---
+++ script.js
@@ -0,0 +1,21 @@
+const mainTodoElem = document.querySelector(".todo-list-elem");
+const inputValue = document.getElementById("inputValue");
+const btn = document.querySelector(".btn");
+
+mainTodoElem.style.display = 'none';
+
+
+
+btn.addEventListener("click", () => {
+    let listPara = document.createElement('p');
+    listPara.setAttribute('class', 'listPara');
+    listPara.innerHTML = `<i class="fa-solid fa-clover"></i>  ${inputValue.value}`;
+    mainTodoElem.style.display = 'block';
+    mainTodoElem.append(listPara);
+    inputValue.value = " ";
+});
+
+mainTodoElem.addEventListener('click', (e) => {
+    let currentElem = e.target;
+    currentElem.remove();
+});
